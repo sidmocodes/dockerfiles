35b45524f1357bf19b99bec0e7f2224cdf3ba006
Siddharth Mohanty <47861446+sidmocodes@users.noreply.github.com>
2023-12-28 12:16:27 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,17 +1,17 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HexFormat;
+import javax.xml.bind.DatatypeConverter;
 
-public class HexToPdfJava11 {
+public class HexToPdfJava8 {
 
     public static void main(String[] args) {
         String inputHexFilePath = "path/to/your/samplehex.txt"; // Path to the hex file
         String outputPdfPath = "path/to/your/output.pdf"; // Path to the output PDF file
 
         try {
-            String hexString = Files.readString(Path.of(inputHexFilePath));
-            byte[] pdfBytes = HexFormat.of().parseHex(hexString);
+            String hexString = new String(Files.readAllBytes(Path.of(inputHexFilePath)));
+            byte[] pdfBytes = DatatypeConverter.parseHexBinary(hexString);
             Files.write(Path.of(outputPdfPath), pdfBytes);
             System.out.println("Hex string converted to PDF and saved to " + outputPdfPath);
         } catch (IOException e) {
